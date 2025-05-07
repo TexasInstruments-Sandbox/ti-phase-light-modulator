4f78c8a9c86b071dc82ed7adee459d85dc9967d5
Sam Martin <martins@ti.com>
2025-05-07 13:54:10 -0500

    only run display test if dependencies were installed

--- tests/test_display.py
+++ tests/test_display.py
@@ -4,7 +4,11 @@ here = pathlib.Path(__file__).parent
 
 
 def test_display():
-    from ti_plm.display import ImageWindow
+    try:
+        from ti_plm.display import ImageWindow
+    except ImportError:
+        print('Skipping test_display: ti_plm installed without `display` module support')
+        return
 
     with ImageWindow() as win:
         win.load(here / '../examples/bird_p67_7cm.png')
